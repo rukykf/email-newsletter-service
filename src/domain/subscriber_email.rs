8800e6656e190e71bf396cea266614b9f03b9a85
Oghenerukevwe Kofi <rukkykofi@pspdfkit.com>
2022-09-12 22:58:37 +0100

    update validation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,10 +1,15 @@
+use validator::validate_email;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
-        // TODO: add validation!
-        Ok(Self(s))
+        if validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
     }
 }
 
@@ -19,8 +24,21 @@ mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
 
+    #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
-        assert_err!(SubscriberEmail::parse(email))
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
     }
 }
